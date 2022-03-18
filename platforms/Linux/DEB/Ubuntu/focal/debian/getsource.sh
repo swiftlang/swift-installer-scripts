6924ad60fde120b83d9ccd9cd743087538dffe9a
Julien Blache <jblache@apple.com>
2022-03-18 16:01:48 -0700

    Debian packaging for Ubuntu 20.04

---
+++ platforms/Linux/DEB/Ubuntu/focal/debian/getsource.sh
@@ -0,0 +1,124 @@
+#!/bin/sh
+
+# This helper script will fetch the Swift sources, save the
+# tarballs following the appropriate naming convention, and
+# build a Debian source package.
+
+# You will need the devscripts and quilt packages installed
+# to run this script and build the packages following these
+# instructions.
+
+# Create swiftlang-X.Y.Z, unpack the debian tarball within,
+# proceed to adding the appropriate entry to the Debian
+# changelog, edit debian/source-version.sh, then run this
+# script from the directory containing swiftlang-X.Y.Z.
+#   $ mkdir swiftlang-X.Y.Z
+#   $ cd swiftlang-X.Y.Z
+#   $ tar xvaf ../swiftlang_A.B.C-D.debian.tar.xz
+#   $ dch -v X.Y.Z-1
+#   $ cd ..
+#   $ swiftlang-X.Y.Z/debian/getsource.sh
+#
+# This will download all the necessary sources, unpack them
+# as required, refresh any Debian patches, then create the
+# Debian source package.
+
+# If all is well, proceed with building the packages.
+#   $ cd swiftlang-X.Y.Z
+#   $ DEB_BUILD_OPTIONS=parallel=64 debuild
+
+set -eu
+
+. $(dirname $0)/source-versions.sh
+
+get_component ()
+{
+    component=$1
+    url="$2"
+
+    dest=swiftlang_${debversion}.orig-${component}
+
+    echo "Downloading ${component} from ${url}"
+
+    case "${url}" in
+	*.zip)
+	    tmpdest=${component}.zip
+	    dest=${dest}.tar.gz
+
+	    curl -L -o ${tmpdest} "${url}"
+	    mk-origtargz --package swiftlang -v ${debversion} -c ${component} --rename --compression gzip ${tmpdest}
+	    ;;
+
+	*.tar.gz)
+	    dest=${dest}.tar.gz
+	    curl -L -o ${dest} "${url}"
+	    ;;
+
+	*.tar.xz)
+	    dest=${dest}.tar.xz
+	    curl -L -o ${dest} "${url}"
+	    ;;
+
+	*.tar.bz2)
+	    dest=${dest}.tar.bz2
+	    curl -L -o ${dest} "${url}"
+	    ;;
+
+	*)
+	    echo "Source archive not in a suitable format for Debian sources: ${url}" >&2
+	    exit 1
+    esac
+
+    mkdir swiftlang-${debversion}/${component}
+    tar -C swiftlang-${debversion}/${component} --strip-components=1 -axf ${dest}
+}
+
+get_component swift https://github.com/apple/swift/archive/swift-${swift_version}.tar.gz
+get_component swift-corelibs-libdispatch https://github.com/apple/swift-corelibs-libdispatch/archive/swift-${swift_version}.tar.gz
+get_component swift-corelibs-foundation https://github.com/apple/swift-corelibs-foundation/archive/swift-${swift_version}.tar.gz
+get_component swift-integration-tests https://github.com/apple/swift-integration-tests/archive/swift-${swift_version}.tar.gz
+get_component swift-corelibs-xctest https://github.com/apple/swift-corelibs-xctest/archive/swift-${swift_version}.tar.gz
+get_component swiftpm https://github.com/apple/swift-package-manager/archive/swift-${swift_version}.tar.gz
+get_component llbuild https://github.com/apple/swift-llbuild/archive/swift-${swift_version}.tar.gz
+get_component cmark https://github.com/apple/swift-cmark/archive/swift-${swift_version}.tar.gz
+get_component swift-xcode-playground-support https://github.com/apple/swift-xcode-playground-support/archive/swift-${swift_version}.tar.gz
+get_component sourcekit-lsp https://github.com/apple/sourcekit-lsp/archive/swift-${swift_version}.tar.gz
+get_component indexstore-db https://github.com/apple/indexstore-db/archive/swift-${swift_version}.tar.gz
+get_component llvm-project https://github.com/apple/llvm-project/archive/swift-${swift_version}.tar.gz
+get_component swift-tools-support-core https://github.com/apple/swift-tools-support-core/archive/swift-${swift_version}.tar.gz
+get_component swift-argument-parser https://github.com/apple/swift-argument-parser/archive/${swift_argument_parser_version}.tar.gz
+get_component swift-driver https://github.com/apple/swift-driver/archive/swift-${swift_version}.tar.gz
+get_component icu https://github.com/unicode-org/icu/archive/release-${icu_version}.tar.gz
+get_component swift-crypto https://github.com/apple/swift-crypto/archive/refs/tags/${swift_crypto_version}.tar.gz
+get_component ninja https://github.com/ninja-build/ninja/archive/refs/tags/v${ninja_version}.tar.gz
+get_component cmake https://github.com/KitWare/CMake/archive/refs/tags/v${cmake_version}.tar.gz
+get_component swift-syntax https://github.com/apple/swift-syntax/archive/swift-${swift_version}.tar.gz
+get_component yams https://github.com/jpsim/Yams/archive/${yams_version}.tar.gz
+get_component swift-atomics https://github.com/apple/swift-atomics/archive/${swift_atomics_version}.tar.gz
+get_component swift-cmark-gfm https://github.com/apple/swift-cmark/archive/swift-${swift_version}-gfm.tar.gz
+get_component swift-docc https://github.com/apple/swift-docc/archive/swift-${swift_version}.tar.gz
+get_component swift-docc-render-artifact https://github.com/apple/swift-docc-render-artifact/archive/swift-${swift_version}.tar.gz
+get_component swift-docc-symbolkit https://github.com/apple/swift-docc-symbolkit/archive/swift-${swift_version}.tar.gz
+get_component swift-collections https://github.com/apple/swift-collections/archive/${swift_collections_version}.tar.gz
+get_component swift-numerics https://github.com/apple/swift-numerics/archive/${swift_numerics_version}.tar.gz
+get_component swift-system https://github.com/apple/swift-system/archive/${swift_system_version}.tar.gz
+get_component swift-nio https://github.com/apple/swift-nio/archive/${swift_nio_version}.tar.gz
+get_component swift-nio-ssl https://github.com/apple/swift-nio-ssl/archive/${swift_nio_ssl_version}.tar.gz
+get_component swift-format https://github.com/apple/swift-format/archive/swift-${swift_version}.tar.gz
+get_component swift-lmdb https://github.com/apple/swift-lmdb/archive/swift-${swift_version}.tar.gz
+get_component swift-markdown https://github.com/apple/swift-markdown/archive/swift-${swift_version}.tar.gz
+
+# Refresh patches, if any
+if [ -s swiftlang-${debversion}/debian/patches/series ]; then
+    cd swiftlang-${debversion}
+
+    export QUILT_PATCHES=debian/patches 
+    export QUILT_REFRESH_ARGS="-p ab --no-timestamps --no-index"
+
+    while quilt push; do quilt refresh; done
+    quilt pop -a
+
+    cd -
+fi
+
+dpkg-source --create-empty-orig -b swiftlang-${debversion}
